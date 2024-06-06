77b55df050f06e174edb24a8d601a1b577bd43ee
amitraut03 <amitraut03@gmail.com>
2024-06-06 23:58:33 +0530

    changes reverted

--- new.py
+++ new.py
@@ -1,1 +1,15 @@
+<<<<<<< HEAD
 ## lost data ###########################
+=======
+print('hello world')
+
+a = 5 
+b = 7
+c = a+b
+
+
+dd = jkjkkjkjfjnfkf
+
+
+ff = jhbbfnfnkflkf
+>>>>>>> parent of 3ce3799 (Update new.py addded)
